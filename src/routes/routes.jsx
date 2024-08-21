ac67914a02eacd8c3b04a09922ea2e74e0ea7d46
Miko Rosales <mikmikinmoko05@gmail.com>
2024-08-21 17:46:45 +0800

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "",
+    element: "Hom",
+    loader: "",
+  },
+  {
+    path: "",
+    element: "",
+    loader: "",
+  },
+  {
+    path: "",
+    element: "",
+    loader: "",
+  },
+  {
+    path: "",
+    element: "",
+    loader: "",
+  },
+]);
+
+export default routes;
